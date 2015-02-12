c8abf5e5f30e0f65c722a785195273f5009d9599
Petroianu Daniel <petroianudaniel@gmail.com>
2015-02-12 15:44:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ function create_user() {
 		sudo dseditgroup -o edit -a ${SERVICE_USER} -t user ${SERVICE_USER}
 	fi
 	SERVICE_CONF=${SERVICE_HOME}/Library/Preferences/org.jenkins-ci.slave.jnlp.conf
-	SERVICE_WRKSPC=${SERVICE_HOME}/Library/Developer/jenkins-ci_slave_wrkspc
+	SERVICE_WRKSPC=${SERVICE_HOME}/_jenkinsci_slave
 }
 
 function install_files() {
